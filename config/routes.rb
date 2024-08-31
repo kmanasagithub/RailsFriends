321371e5127181f10449b2042e140dc6c0e6383b
kurella manasa <kurellamanasa204@gmail.com>
2024-08-31 10:53:27 +0530

    first Commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # Defines the root path route ("/")
+  get "home/about"
+  root "friends#index"
+end
